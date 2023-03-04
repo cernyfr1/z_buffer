895c53e1f3462d533a3d0b61a640a52bfddeca19
František Černý <fa.cerny@gmail.com>
2023-03-04 21:05:02 +0100

    Fix rasterization bug in Triangler

--- src/rasterops/Triangler.java
+++ src/rasterops/Triangler.java
@@ -2,10 +2,7 @@ package rasterops;
 
 import linalg.Lerp;
 import objectdata.Vertex;
-import rasterdata.ColorRaster;
-import rasterdata.Raster;
 import rasterdata.ZBuffer;
-import transforms.Col;
 
 import java.util.Comparator;
 import java.util.List;
@@ -14,7 +11,6 @@ import java.util.stream.Stream;
 public class Triangler {
 
     private final Lerp lerp = new Lerp();
-    //private final Raster<Col> colorRaster;
     private final ZBuffer zBuffer;
 
     public Triangler(ZBuffer zBuffer) {
@@ -38,20 +34,22 @@ public class Triangler {
         final int yMin = (int)a.getPosition().getY();
         final double yMax = b.getPosition().getY();
 
-        for (int y = yMin; y < yMax; y++){
+        for (int y = yMin; y < yMax; y++) {
             final double t1 = (y - a.getPosition().getY()) / (c.getPosition().getY() - a.getPosition().getY());
             final double t2 = (y - a.getPosition().getY()) / (b.getPosition().getY() - a.getPosition().getY());
-            final Vertex v1 = lerp.compute(a, c, t1);
-            final Vertex v2 = lerp.compute(a, b, t2);
-            final Vertex vMin = (v1.getPosition().getX() < v2.getPosition().getX())? v1 : v2;
-            final Vertex vMax = (vMin == v1)? v2 : v1;
-            final int xMin = (int) vMin.getPosition().getX();
-            final double xMax = vMax.getPosition().getX();
+            if (t1 > 0 && t2 > 0) {
+                final Vertex v1 = lerp.compute(a, c, t1);
+                final Vertex v2 = lerp.compute(a, b, t2);
+                final Vertex vMin = (v1.getPosition().getX() < v2.getPosition().getX()) ? v1 : v2;
+                final Vertex vMax = (vMin == v1) ? v2 : v1;
+                final int xMin = (int) vMin.getPosition().getX();
+                final double xMax = vMax.getPosition().getX();
 
-            for (int x = xMin; x < xMax; x++){
-                final double t = (x - xMin) / (xMax - xMin);
-                final Vertex v = lerp.compute(vMin, vMax, t);
-                zBuffer.setPixel(x, y, v.getPosition().getZ(), v.getColor());
+                for (int x = xMin; x < xMax; x++) {
+                    final double t = (x - xMin) / (xMax - xMin);
+                    final Vertex v = lerp.compute(vMin, vMax, t);
+                    zBuffer.setPixel(x, y, v.getPosition().getZ(), v.getColor());
+                }
             }
         }
     }
@@ -59,21 +57,22 @@ public class Triangler {
     void drawSecondHalf(Vertex a, Vertex b, Vertex c){
         final int yMin = (int)b.getPosition().getY();
         final double yMax = c.getPosition().getY();
-
-        for (int y = yMin; y < yMax; y++){
+        for (int y = yMin; y < yMax; y++) {
             final double t1 = (y - a.getPosition().getY()) / (c.getPosition().getY() - a.getPosition().getY());
             final double t2 = (y - b.getPosition().getY()) / (c.getPosition().getY() - b.getPosition().getY());
-            final Vertex v1 = lerp.compute(a, c, t1);
-            final Vertex v2 = lerp.compute(b, c, t2);
-            final Vertex vMin = (v1.getPosition().getX() < v2.getPosition().getX())? v1 : v2;
-            final Vertex vMax = (vMin == v1)? v2 : v1;
-            final int xMin = (int) vMin.getPosition().getX();
-            final double xMax = vMax.getPosition().getX();
+            if (t1 > 0 && t2 > 0) {
+                final Vertex v1 = lerp.compute(a, c, t1);
+                final Vertex v2 = lerp.compute(b, c, t2);
+                final Vertex vMin = (v1.getPosition().getX() < v2.getPosition().getX()) ? v1 : v2;
+                final Vertex vMax = (vMin == v1) ? v2 : v1;
+                final int xMin = (int) vMin.getPosition().getX();
+                final double xMax = vMax.getPosition().getX();
 
-            for (int x = xMin; x < xMax; x++){
-                final double t = (x - xMin) / (xMax - xMin);
-                final Vertex v = lerp.compute(vMin, vMax, t);
-                zBuffer.setPixel(x, y, v.getPosition().getZ(), v.getColor());
+                for (int x = xMin; x < xMax; x++) {
+                    final double t = (x - xMin) / (xMax - xMin);
+                    final Vertex v = lerp.compute(vMin, vMax, t);
+                    zBuffer.setPixel(x, y, v.getPosition().getZ(), v.getColor());
+                }
             }
         }
     }
